cae35f547cae799855d920ec93d9c947a34edcb5
Daniel Bullock <dnbulloc@iu.edu>
2019-11-15 16:15:23 -0500

    first commit

---
+++ main.m
@@ -0,0 +1,28 @@
+function main()
+
+if ~isdeployed
+    disp('\n adding paths');
+    addpath(genpath('/N/u/brlife/git/jsonlab'))
+    addpath(genpath('/N/u/brlife/git/vistasoft'))
+    addpath(genpath('/N/u/brlife/git/wma_tools'))
+end
+
+config = loadjson('config.json');
+wbFG = fgRead(config.track);
+load(config.classification)
+
+tractStruc = bsc_makeFGsFromClassification_v4(classification, wbFG);
+
+tractStruc{1}.name
+   for itracts=1:length(classification.names)
+      [~] = dtiExportFibersMrtrix(tractStruc{itracts}, strcat('output/tcks/',tractStruc{itracts}.name,'.tck'));
+   end
+
+for it = 1:length(tractStruc)
+    all_tracts(it).name = tractStruc{it}.name;
+    all_tracts(it).color = tractStruc{it}.colorRgb;
+    all_tracts(it).filename = sprintf('%s.json',tractStruc{it}.name);
+end
+savejson('', all_tracts, fullfile(tractspath, 'tracts.json'));
+
+end
